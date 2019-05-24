00ba9f8b6f08a0595176e1bb370c12e71f505590
Isabella Garcia-Camargo <isabellagc2010@gmail.com>
2019-05-24 12:57:54 -0700

    dataframes ready for embedding

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt # plotting
 import numpy as np # linear algebra
 import os # accessing directory structure
+from sklearn.model_selection import train_test_split
 
 import json
 
@@ -42,13 +43,82 @@ def readFoodRecData(version):
     print(df.head())
 
 
-@cli.command()
+# @cli.command()
 def get_json_recipes():
+    print("parsing out the recipes...")
     with open('data/epicurious/full_format_recipes.json', 'r') as f:
         recipe_ingredients_dict = json.load(f)
 
     for i in range(10):
-        print(recipe_ingredients_dict[i])
+        print(recipe_ingredients_dict[i]['ingredients'][0])
+        print('=' * 100)
+
+    return recipe_ingredients_dict
+
+
+@cli.command()
+def vectorize():
+    json_recipes = get_json_recipes()
+    recipes = pd.DataFrame.from_dict(json_recipes, orient='columns')
+    
+    # Not tags
+    recipe_tags = recipes.drop(['title', 'calories', 'protein', 'fat', 'sodium'], axis = 1)
+    mean_rating = recipes['rating'].mean()
+    #creating new column where ratings are 1 for good and 0 for bad
+    recipes['target'] = np.where(recipes['rating']>=mean_rating, 1, 0)
+
+    print("MEAN RATING: "  + str(mean_rating))
+    print str(recipes.head())
+    print "COLUMNS" + str(recipes.columns)
+
+    #FOR JOSEPH
+    ingred_to_rating = pd.concat((recipes['ingredients'], recipes['rating'], recipes['target']), axis=1, keys=['ingredients', 'rating', 'target'])
+    print ('=' * 100)
+    print "dataframe for joseph"
+    print ('=' * 100)
+    print str(ingred_to_rating.head())
+    print ('=' * 100)
+    print ('=' * 100)
+
+
+
+    # Create data and target, split into train and test, I think we should sample evenly
+    recipe_tags.target = recipe_tags['rating']
+    recipe_tags.target = recipe_tags.target.round(0) #MAYBE CHANGE THIS
+    recipe_tags.target = pd.to_numeric(recipe_tags.target, downcast='integer')
+    #print(recipe_tags.target.head())
+    print(recipe_tags.target.value_counts())
+    recipe_tags.data = recipe_tags.drop(['rating'], axis = 1)
+    x_train, x_test, y_train, y_test = train_test_split(recipe_tags.data, recipe_tags.target, test_size=0.3, random_state=42)
+
+    
+
+
+
+    # Counter = Counter(split_it)
+    # most_occur = Counter.most_common(2000) # TODO: change number
+    # most_common_words = [word for word, word_count in most_occur]
+    # with open('dict.csv', 'w') as f:
+    #     for item in most_common_words:
+    #         f.write("%s\n" % (str(item)))
+
+    # #create bag-of-words style vectors for training and test sets, 
+    # with open('train_vec.csv', 'w') as f:
+    #     clean_ing = pd.read_csv('something.csv') # read in list of 
+    #     for ingredient in clean_ing['content'].tolist():
+    #         l = [];
+    #         for word in most_common_words:
+    #             if str(word) in str(cont):
+    #                 l.append(1)
+    #             else:
+    #                 l.append(0)
+    #         f.write("0")
+    #         for word in l:
+    #             f.write(",%s" % (word))
+    #         f.write("\n")
+
+
+
 
 
 #import Epicurious (tags) dataset
