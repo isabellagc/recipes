6f4b12c5dd5ca5fefc0838acb0036c06c0a5e740
Joseph Matan <jmatan20@stanford.edu>
2019-05-24 10:48:02 -0700

    Added multiple logistic regression

--- logreg.py
+++ logreg.py
@@ -27,52 +27,31 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 
 
-def readFoodRecData():
-    print("Attempting to read data from foodRecSys package...")
-    df = data_utils.get_full_core_df()
-    df.head()
-
-def getInputs():
-    global filename
-    while True:
-        pass
-        try:
-            filename = str(input('Enter dataset filepath.\n'))
-            break
-        except:
-            print("That's not a valid option!\n")
-            sys.stderr.close()
-
-getInputs()
-
 #import Epicurious (tags) dataset
-#recipes =pd.read_csv("/Users/josephmatan/downloads/epi2/epi_r.csv").dropna()
-recipes =pd.read_csv(filename).dropna()
+recipes = pd.read_csv('./data/epicurious/epi_r.csv').dropna()
 
-mean_rating = recipes['rating'].mean()
-#recipes['target'] = np.where(recipes['rating']>=mean_rating, 1, 0)
-recipes['target'] = np.where(recipes['rating']>= 4, 1, 0)  # TODO: decide on what is a 'positive' rating
-#print(mean_rating)
-#print(recipes.head())
+# Not tags
+recipe_tags = recipes.drop(['title', 'calories', 'protein', 'fat', 'sodium'], axis = 1)
 
-recipe_tags = recipes.drop(['title', 'rating', 'calories', 'protein', 'fat', 'sodium'], axis = 1)
-#print(recipe_tags.head())
-recipe_tags.target = recipe_tags['target']
-recipe_tags.data = recipe_tags.drop(['target'], axis = 1)
+# Create data and target, split into train and test, I think we should sample evenly
+recipe_tags.target = recipe_tags['rating']
+recipe_tags.target = recipe_tags.target.round(0)
+recipe_tags.target = pd.to_numeric(recipe_tags.target, downcast='integer')
 #print(recipe_tags.target.head())
-#print(recipe_tags.data.head())
+print(recipe_tags.target.value_counts())
+recipe_tags.data = recipe_tags.drop(['rating'], axis = 1)
+x_train, x_test, y_train, y_test = train_test_split(recipe_tags.data, recipe_tags.target, test_size=0.3, random_state=42)
 
-x_train, x_test, y_train, y_test = train_test_split(recipe_tags.data, recipe_tags.target, test_size=0.30, random_state=0)
+# Run logistic regression, print results
+mult_log_reg = LogisticRegression(multi_class = 'auto', solver = 'liblinear', max_iter = 1000)
+mult_log_reg.fit(x_train, y_train)
+print('multiple logistic regression:')
+predictions = mult_log_reg.predict(x_test)
+print('Score:')
+print(mult_log_reg.score(x_test, y_test),'\n')
+print('Confusion Matrix')
+print(metrics.confusion_matrix(y_test, predictions))
 
-log_reg = LogisticRegression(solver = 'lbfgs')
-log_reg.fit(x_train, y_train)
-predictions = log_reg.predict(x_test)
-#print(predictions.head())
-
-score = log_reg.score(x_test, y_test)
-print(score)
-cm = metrics.confusion_matrix(y_test, predictions)
-print(cm)
 
 
 
