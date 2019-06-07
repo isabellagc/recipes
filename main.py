89cea924530356575a5400f6b656d4b15671454a
Isabella Garcia-Camargo <isabellagc2010@gmail.com>
2019-06-07 15:21:42 -0700

    cant add pckl to the repo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 import pandas as pd
-
+import math 
 import tensorflow as tf
 
 from tensorflow import feature_column
@@ -19,7 +19,7 @@ import sys
 #sys.setdefaultencoding('utf-8')
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestClassifier     
 from sklearn import preprocessing
 
@@ -365,75 +365,177 @@ def demo(feature_column):
   print(feature_layer(example_batch).numpy())
 
 
+@cli.command()
+@click.option('--vals', default=100, help='how many of top words to include') #how manhy of the top 5000 words to take ink
+def finalDF(vals):
+     #real df with tags 
+    recipes = pd.read_csv('data/epicurious/epi_r.csv')
+
+    #just the ingredient part 
+    json_recipes = get_json_recipes()
+    ingredients = pd.DataFrame.from_dict(json_recipes, orient='columns')
+    ingredients = ingredients.dropna(subset=['rating', 'ingredients'])
+    ingredients['ingredients'] = ingredients['ingredients'].apply(removeNums)
+    #NOW CHANGE FROM VEC OF STRINGS TO ONE FAT STRING
+    ingredient_string = ingredients['ingredients'].apply(lambda x : " ".join(str(word) for word in x))
+    
+
+    print('='*80)
+    # get most common bigrams
+    # Now this also takes out stop words
+    sw = stopwords.words('english')
+    sw.append('andor')
+
+    v = CountVectorizer(ngram_range=(1, 2), stop_words = sw)
+    bigrams = v.fit_transform(ingredient_string)
+    vocab = v.vocabulary_
+    count_values = bigrams.toarray().sum(axis=0)
+    print('='*80)
+    # Make list of 5000 most common words and bigrams
+    i = 0
+    most_common_words = []
+    for bg_count, bg_text in sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)[0:vals]:
+        print (bg_count, bg_text)
+        most_common_words.append(bg_text)
+    print('\nVector of most common words and bigrams is this long: ' + str(len(most_common_words)))
+
+
+    # Makes a feature vector for each list of ingredients
+    feature_vectors = []
+    for i, row in ingredients.iterrows():
+        feature_vec = []
+        ingred = str(row['ingredients'])
+        for word in most_common_words:
+            if word in ingred:
+                feature_vec.append(1)
+            else:
+                feature_vec.append(0)
+        feature_vectors.append(feature_vec)
+
+    ingr =  pd.DataFrame(feature_vectors, columns = most_common_words)
+    print("this is the shape of the ingredient matrix: " + str(ingr.shape))
+    print(ingr.head())
+    # ingr['rating'] = recipes['rating']
+    combined_df = pd.concat([recipes, ingr], axis=1)
+    print("this is the new matrix shape: " + str(combined_df.shape))
+    combined_df = combined_df.dropna()
+    #TODO: dropping the title until we can embed it
+    final = combined_df.drop(['title'], axis = 1)
+    print(final.head())
+    print("this is the final matrix shape without null: " + str(final.shape))
+    final.to_pickle('final_dataframe.pkl')
+
 
 
 @cli.command()
 def neuralnetfiltered():
-    #import Epicurious (tags) dataset
-    recipes = pd.read_csv('data/epicurious/epi_r.csv').dropna()
-    print(recipes.columns)
-
-    center = recipes[['calories', 'protein', 'fat', 'sodium']]
-    print('CENTERED'*50)
-    print(center)
-    print('MEANS'*20)
-    colmeans = center.mean(axis = 0) 
-    print(colmeans)
-    print('MEDIANS' * 20)
-    colmeadians = center.median(axis = 0)
-    print(colmeadians)
-    # mean_centered = center - colmeans
-    # mean_centered['rating'] = recipes['rating']
-    # mean_centered['rating'] = recipes['rating'].values
-
-    #take out extraneous vals
-    center['rating'] = recipes['rating']
-    center['rating'] = recipes['rating'].values
-    center_filtered = recipes[ (abs(recipes['calories'] - colmeadians['calories']) < colmeadians['calories']) & (abs(recipes['sodium'] - colmeadians['sodium']) < colmeadians['sodium'])]
-    center_filtered = center_filtered.drop(['title'], axis = 1)
-    print(center_filtered)
-
-    counter = 0
-    for index, row in center_filtered.iterrows():
-        if row['calories'] > 6350.682993:
-            print(index, row['calories'])
-            counter += 1
-    print('counter ' * 100)
-    print(counter)
-
-    center_filtered_vals = center_filtered[['calories', 'protein', 'fat', 'sodium']]
-    print('MEANS'*20)
-    colmeans_filtered = center_filtered_vals.mean(axis = 0) 
-    print(colmeans_filtered)
-    print('MEDIANS' * 20)
-    colmeadians_filtered = center_filtered_vals.median(axis = 0)
-    print(colmeadians_filtered)
-
-    mean_centered = center_filtered_vals - colmeans_filtered
-    mean_centered['rating'] = center_filtered['rating']
-    mean_centered['rating'] = center_filtered['rating'].values
+    recipes = pd.read_pickle('final_dataframe.pkl')
+    #######################
 
 
+    recipes.target = recipes['rating']
+    recipes.data = recipes.drop(['rating'], axis = 1)
+    # recipe_filtered.data = recipe_filtered['protein']
+
+    
+    x_train, x_test, y_train, y_test = train_test_split(recipes.data, recipes.target, test_size=0.3, random_state=42)
+    x_train, x_valid, y_train, y_valid = train_test_split(recipes.data, recipes.target, test_size=0.3, random_state=42)
+    #TODO: center the shits (calories)
+
+    # Define model
+    model = Sequential()
+    model.add(Dense(50, input_dim=778, activation= "relu"))
+    model.add(Dense(50, activation= "relu"))
+    model.add(Dense(1))
+    model.summary() #Print model Summary
+
+    # Compile model
+    model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
+    
+    # Fit Model
+    model_output = model.fit(x_train, y_train, epochs=150, batch_size = 20, verbose=1, validation_data = (x_valid, y_valid))
+    #TODO: look into tthis stuff so we can use it, visualize in here 
+    # print("Training accuracy: " , np.mean(model_output.history['acc']))
+    # print("Validation accuracy: " , np.mean(model_output.history['val_acc']))
+
+    prediction_nn= model.predict(x_valid)
+    score = np.sqrt(mean_squared_error(prediction_nn,y_valid))
+    print ("neural network mean square", score)
+    score2 = r2_score(y_valid,prediction_nn)
+    print("neural network r2", score2)
 
 
+    #random forest code
+    random.seed(42)
+    rf = RandomForestRegressor(n_estimators=10)
+    rf.fit(x_train, y_train)
+    print("fit to random forest")
+    y_valid_rf = rf.predict(x_valid)
+    score = np.sqrt(mean_squared_error(y_valid, y_valid_rf))
+    print ("random forest mean square", score)
+    score2 = r2_score(y_valid, y_valid_rf)
+    print("random forest r2", score2)
+
+
+    quit()
+     
+
+
+    # #import Epicurious (tags) dataset
+    # se = pd.Series(feature_vectors)
+    # recipes['feature'] = se.values
+    # # mean_rating = recipes['rating'].mean()
+    # # recipes['target'] = np.where(recipes['rating']>=mean_rating, 1, 0)
+    
+    # center = recipes[['calories', 'protein', 'fat', 'sodium']]
     # print('CENTERED'*50)
     # print(center)
-    # print('CENTERED'*50)
+    # print('MEANS'*20)
+    # colmeans = center.mean(axis = 0) 
+    # print(colmeans)
+    # print('MEDIANS' * 20)
+    # colmeadians = center.median(axis = 0)
+    # print(colmeadians)
+
+
+    # #take out extraneous vals
+    # center['rating'] = recipes['rating']
+    # center['rating'] = recipes['rating'].values
+    # center_filtered = recipes[ (abs(recipes['calories'] - colmeadians['calories']) < colmeadians['calories']) & (abs(recipes['sodium'] - colmeadians['sodium']) < colmeadians['sodium'])]
+    # center_filtered = center_filtered.drop(['title'], axis = 1)
+    # print(center_filtered)
+
     # counter = 0
-    # for index, row in center.iterrows():
-    #     if row['calories'] <= 0:
+    # for index, row in center_filtered.iterrows():
+    #     if row['calories'] > 6350.682993:
     #         print(index, row['calories'])
     #         counter += 1
     # print('counter ' * 100)
     # print(counter)
-    # mean_centered = preprocessing.scale(center, with_mean='True', with_std='False')
-    # #convert back into a Pandas dataframe and add column names
-    # mean_centered_df = pd.DataFrame(mean_centered, columns=center.columns)
-    # mean_centered_df['rating'] = recipes['rating']
-    # mean_centered_df['rating'] = recipes['rating'].values
+
+    # center_filtered_vals = center_filtered[['calories', 'protein', 'fat', 'sodium', 'feature']]
+    # print('MEANS'*20)
+    # colmeans_filtered = center_filtered_vals.mean(axis = 0) 
+    # print(colmeans_filtered)
+    # print('MEDIANS' * 20)
+    # colmeadians_filtered = center_filtered_vals.median(axis = 0)
+    # print(colmeadians_filtered)
+
+    # mean_centered = center_filtered_vals - colmeans_filtered
+    # mean_centered['rating'] = center_filtered['rating']
+    # mean_centered['rating'] = center_filtered['rating'].values
+
+
+
+
+
+    #import Epicurious (tags) dataset
+    recipes = pd.read_csv('data/epicurious/epi_r.csv').dropna()
+
+
 
     # Not tags
-    recipe_filtered= mean_centered
+    recipe_filtered= recipes.drop(['calories', 'protein', 'fat', 'sodium', 'title'], axis = 1)
     print(recipe_filtered)
     recipe_tags = recipe_filtered.drop(['rating'], axis = 1)
     print('TAGS ' * 50)
@@ -452,37 +554,9 @@ def neuralnetfiltered():
     x_train, x_valid, y_train, y_valid = train_test_split(recipe_filtered.data, recipe_filtered.target, test_size=0.3, random_state=42)
 
 
-
-
-    # # Not tags
-    # recipe_filtered= recipes.drop(['calories', 'protein', 'fat', 'sodium', 'title'], axis = 1)
-    # print(recipe_filtered)
-    # recipe_tags = recipe_filtered.drop(['rating'], axis = 1)
-    # print('TAGS ' * 50)
-    # print(recipe_tags)
-    # print('----'*20)
-    # print('----'*20)
-    # # Create data and target, split into train and test, I think we should sample evenly
-
-    # recipe_filtered.target = recipe_filtered['rating']
-    # recipe_filtered.data = recipe_filtered.drop(['rating'], axis = 1)
-    # # recipe_filtered.data = recipe_filtered['protein']
-
-    # print(recipe_filtered.target)
-
-    # x_train, x_test, y_train, y_test = train_test_split(recipe_filtered.data, recipe_filtered.target, test_size=0.3, random_state=42)
-    # x_train, x_valid, y_train, y_valid = train_test_split(recipe_filtered.data, recipe_filtered.target, test_size=0.3, random_state=42)
-
-
-
-    # random.seed(42)
-    # rf = RandomForestRegressor(n_estimators=10)
-    # rf.fit(x_train, y_train)
-    # print("fit to random forest")
-
     # Define model
     model = Sequential()
-    model.add(Dense(100, input_dim=4, activation= "relu"))
+    model.add(Dense(100, input_dim=674, activation= "relu"))
     model.add(Dense(100, activation= "relu"))
     model.add(Dense(1))
     model.summary() #Print model Summary
@@ -497,16 +571,24 @@ def neuralnetfiltered():
     # print("Validation accuracy: " , np.mean(model_output.history['val_acc']))
 
     pred= model.predict(x_valid)
-    print(pred)
-    score = np.sqrt(mean_squared_error(y_valid,pred))
-    print ("neural network", score)
+    score = np.sqrt(mean_squared_error(pred,y_valid))
+    print ("neural network mean square", score)
+    score2 = r2_score(y_valid,pred)
+    print("neural network r2", score2)
 
 
 
-    # #Prediction using Random Forest 
-    # y_valid_rf = rf.predict(x_valid)
-    # score = np.sqrt(mean_squared_error(y_valid_rf,pred))
-    # print ("random forest", score)
+    #random forest code:
+    random.seed(42)
+    rf = RandomForestRegressor(n_estimators=10)
+    rf.fit(x_train, y_train)
+    print("fit to random forest")
+    y_valid_rf = rf.predict(x_valid)
+    score = np.sqrt(mean_squared_error(y_valid, y_valid_rf))
+    print ("random forest mean square", score)
+    score2 = r2_score(y_valid, y_valid_rf)
+    print("random forest r2", score2)
+
 
 
 
@@ -574,7 +656,7 @@ def neuralnet():
 
     #Prediction using Random Forest 
     y_valid_rf = rf.predict(x_valid)
-    score = np.sqrt(mean_squared_error(y_valid_rf,pred))
+    score = np.sqrt(mean_squared_error(y_valid_rf,y_valid))
     print ("random forest", score)
 
 
